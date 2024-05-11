b0d85f8a89e1f5f90dc8d566d5fbc6dcb9483286
Jonius30 <jonius30@gmail.com>
2024-05-11 12:35:06 +0200

    Init Front

---
+++ BibliotheksVerwaltung/BibliotheksVerwaltungWEB/Program.cs
@@ -0,0 +1,11 @@
+using BibliotheksVerwaltungWEB;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]?.ToString() ?? string.Empty) });
+
+await builder.Build().RunAsync();
