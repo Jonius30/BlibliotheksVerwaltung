d407f11acb398e18ab455037b99ec33278536c5a
Jonius30 <jonius30@gmail.com>
2024-05-17 08:05:34 +0200

    Add Api

---
+++ BibliotheksVerwaltung/BibliotheksVerwaltungLib/Interfaces/IAutorService.cs
@@ -0,0 +1,21 @@
+﻿using BibliotheksVerwaltungLib.Models.Dtos;
+using BibliotheksVerwaltungLib.Models.Request;
+using BibliotheksVerwaltungLib.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotheksVerwaltungLib.Interfaces
+{
+    public interface IAutorService
+    {
+        public Task<List<AutorDto>> ListAsnyc();
+        public Task<AutorDto> GetByIDAsync(AutorByIdRequest request);
+
+        public Task<AutorDto> CreateAsync(AutorCreateRequest request);
+        public Task<AutorDto> UpdateAsync(AutorUpdateRequest request);
+        public Task DeleteAsync(int autorId);
+    }
+}
--- BibliotheksVerwaltung/BibliotheksVerwaltungWEB/Program.cs
+++ BibliotheksVerwaltung/BibliotheksVerwaltungWEB/Program.cs
@@ -1,4 +1,6 @@
+using BibliotheksVerwaltungLib.Interfaces;
 using BibliotheksVerwaltungWEB;
+using BibliotheksVerwaltungWEB.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +9,5 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]?.ToString() ?? string.Empty) });
-
+builder.Services.AddScoped<IAutorService, AutorService>();
 await builder.Build().RunAsync();
